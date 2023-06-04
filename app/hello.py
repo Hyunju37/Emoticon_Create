e16447ac783f2b4b8f7f806bb675b89ac8d7365b
kingjiwoo <jaydenkim2931@gmail.com>
2023-06-04 13:33:11 +0900

    URL구성

--- app/hello.py
+++ app/hello.py
@@ -6,3 +6,23 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+@app.route('/concept')
+def concept():
+    return '컨셉을 입력하세요 '
+
+@app.route('/personalize')
+def personalize():
+    return '추가 개인화 여부'
+
+@app.route('/emotion')
+def emotion():
+    return '감정 구성하기'
+
+@app.route('/emotion/specific')
+def specific():
+    return '감정 별 묘사하기'
+
+@app.route('/result')
+def result():
+    return '결과 확인'
