1d667ee6ef822670e8cd71c845520f6bb7d5b827
kingjiwoo <jaydenkim2931@gmail.com>
2023-06-07 15:38:29 +0900

    감정묘사 동적 test

--- app/app.py
+++ app/app.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template,request,redirect,url_for
 import sys
-import database
 from form import ConceptForm,EmotionCount,EmotionDescribe
 
-con=ConceptForm()
-app=Flask(__name__)
+
+app = Flask(__name__)
 
 @app.route("/")
 def hello():
