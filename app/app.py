bbfa3e9be336ea7c19bf8a15516a68c201b087c8
ysc <ysc9692@naver.com>
2023-06-07 11:09:18 +0900

    윤수 작업물 병합

---
+++ app/app.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template,request,redirect,url_for
+import sys
+import database
+from form import ConceptForm,EmotionCount,EmotionDescribe
+
+con=ConceptForm()
+app=Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template("homepage.html")
+
+@app.route("/Genemo")
+def Genemo():
+    return redirect(url_for('hello'))
+
+
+@app.route("/start")
+def start():
+    return render_template("start.html")
+
+@app.route("/contribution")
+def contribution():
+    return render_template("contribution.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/applyconcept")
+def applyconcept():
+    inputconcept=request.args.get("inputconcept")
+    print(inputconcept)
+    # database.save(inputconcept)
+    return render_template("emotion.html")
+
+@app.route("/applyemotion")
+def applyemotion():
+    emotion1=request.args.get("inputemotion1")
+    emotion2=request.args.get("inputemotion2")
+    emotion3=request.args.get("inputemotion3")
+    emotion4=request.args.get("inputemotion4")
+    emotion5=request.args.get("inputemotion5")
+    emotion6=request.args.get("inputemotion6")
+    emotion7=request.args.get("inputemotion7")
+    print(emotion1,emotion2,emotion3,emotion4,emotion5,emotion6,emotion7)
+    # database.save(emotion1,emotion2,emotion3,emotion4,emotion5,emotion6,emotion7)
+    # return render_template("emotion.html")
+
+
+if __name__=="__main__":
+    app.run()
