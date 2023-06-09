64cfff5c104d8ccb45b6a485e30eb6272f6a025f
kingjiwoo <jaydenkim2931@gmail.com>
2023-06-09 11:19:48 +0900

    chatGPT 모듈 추가

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template,request,redirect,url_for
 import sys
 from form import ConceptForm,EmotionCount,EmotionDescribe
+
 import ast
 # con=ConceptForm()
 # con1=EmotionCount()
