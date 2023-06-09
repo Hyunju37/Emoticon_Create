859df97cffad76191e91b1a2b6a74ddd2109a5d3
ysc <ysc9692@naver.com>
2023-06-09 16:25:39 +0900

    추가 수정

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, session,request
+from flask import Flask, render_template, redirect, url_for, session
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, IntegerField, TextAreaField
 from wtforms.validators import InputRequired, NumberRange, ValidationError
@@ -11,43 +11,6 @@ def save_data_to_file(data):
     filename = 'data.txt'
     with open(filename, 'a') as file:
         file.write(data + '\n')
-
-class ConceptForm(FlaskForm):
-    concept = StringField('개념', validators=[InputRequired()])
-
-class PersonalizeStatus(FlaskForm):
-    status = SelectField('개인화 상태', choices=[('yes', '예'), ('no', '아니오')])
-
-class EmotionCount(FlaskForm):
-    happiness = IntegerField('Happiness', validators=[NumberRange(min=0, max=32)])
-    sadness = IntegerField('Sadness', validators=[NumberRange(min=0, max=32)])
-    anger = IntegerField('Anger', validators=[NumberRange(min=0, max=32)])
-    disgust = IntegerField('Disgust', validators=[NumberRange(min=0, max=32)])
-    neutral = IntegerField('Neutral', validators=[NumberRange(min=0, max=32)])
-    surprise = IntegerField('Surprise', validators=[NumberRange(min=0, max=32)])
-    fear = IntegerField('Fear', validators=[NumberRange(min=0, max=32)])
-
-    def validate(self, extra_validators=None):
-        if not super().validate(extra_validators):
-            return False
-        total = (
-            self.happiness.data + self.sadness.data + self.anger.data +
-            self.disgust.data + self.neutral.data + self.surprise.data +
-            self.fear.data
-        )
-        if total != 32:
-            raise ValidationError('감정 개수의 합은 반드시 32여야 합니다.')
-        return True
-
-class EmotionDescribe(FlaskForm):
-    happiness_description = TextAreaField('기쁨 감정 설명', validators=[InputRequired()])
-    sadness_description = TextAreaField('슬픔 감정 설명', validators=[InputRequired()])
-    anger_description = TextAreaField('분노 감정 설명', validators=[InputRequired()])
-    disgust_description = TextAreaField('혐오 감정 설명', validators=[InputRequired()])
-    neutral_description = TextAreaField('중립 감정 설명', validators=[InputRequired()])
-    surprise_description = TextAreaField('놀람 감정 설명', validators=[InputRequired()])
-    fear_description = TextAreaField('두려움 감정 설명', validators=[InputRequired()])
-
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'genemo'
